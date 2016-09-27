b3049a00679d563007d072147e6f0fb1773272bd
Michael Shihrer <michael@shihrer.me>
2016-09-27 03:12:51 -0600

    Completed MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * Michael Manning
  * Michael Shihrer
@@ -10,6 +12,36 @@ class MergeSort implements SortStrategy {
     }
 
     private int[] mergeSort(int[] input){
-        return new int[0];
+        // Base case
+        if(input.length <= 1)
+            return input;
+
+        // Divide problem
+        int[] leftHalf = Arrays.copyOfRange(input, 0, input.length/2);
+        int[] rightHalf = Arrays.copyOfRange(input, input.length/2, input.length);
+
+        // Conquer problem
+        leftHalf = mergeSort(leftHalf);
+        rightHalf = mergeSort(rightHalf);
+        // Return merged results
+        return merge(leftHalf, rightHalf);
+    }
+
+    private int[] merge(int[] a, int[] b){
+        int[] temp = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] <= b[j])
+                temp[k++] = a[i++];
+            else
+                temp[k++] = b[j++];
+        }
+
+        while(i < a.length)
+            temp[k++] = a[i++];
+        while(j < b.length)
+            temp[k++] = b[j++];
+
+        return temp;
     }
 }
