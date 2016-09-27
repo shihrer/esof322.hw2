a9d7378dc2cb9301c5699f055ec61825e3abcecf
Michael Shihrer <michael@shihrer.me>
2016-09-27 03:18:57 -0600

    Completed Insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -10,6 +10,16 @@ class InsertionSort implements SortStrategy {
     }
 
     private int[] insertionSort(int[] input){
-        return new int[0];
+        for(int i = 1; i < input.length; i++){
+            int j = i;
+            while(j > 0 && input[j-1] > input[j]){
+                //swap
+                int temp = input[j];
+                input[j] = input[j-1];
+                input[j-1] = temp;
+                j--;
+            }
+        }
+        return input;
     }
 }
